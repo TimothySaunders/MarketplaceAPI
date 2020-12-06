1f310cf64bae0a333adcd5dbbfbe2e7c13325294
Timothy Saunders <timsaunders88@gmail.com>
2020-12-06 13:34:28 +0000

    added ProductController with GET route, v1/products

---
+++ MarketplaceAPI/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MarketplaceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketplaceAPI.Controllers
+{
+    [Route("v1")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        // GET: products
+        [HttpGet("products")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+    }
+}
